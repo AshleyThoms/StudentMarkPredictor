f4eaf4a8601f97ec5e783d407941b17bcca4e79c
Ashley Jean Thomas <ashleythoms.co@gmail.com>
2026-04-23 21:55:31 +0800

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+
+st.set_page_config(page_title=" Student Marks Prediction",page_icon="😬",layout="centered")
+
+st.title("🤖🤖 Student Marks Predictor 🤖🤖")
+st.write("Enter The Number Od Hours Studied (1-10) And **Click Predict** To See The Predicted Mark")
+
+# Load The Model
+
+def load_model(model):
+    with open(model,"rb") as f:
+        slr = pickle.load(f)
+    return slr
+
+try:
+    model = load_model("slr.pkl")
+except Exception as e:
+    st.error("Your Pickle File Not Found")
+    st.exception("Failed To Load The Model : ",e)
+    st.stop()
+
+hours = st.number_input("Hours Studied",
+                        min_value=1.0,
+                        max_value=10.0,
+                        value=4.0,
+                        step=0.1,
+                        format="%.1f"
+                        )
+
+if st.button("Predict"):
+    try:
+        x = np.array([[hours]])
+        predictions = model.predict(x)
+        predictions = predictions[0]
+        st.success(f" Predicted Marks : {predictions:.1f}")
+        st.write(" Note : This Is ML Model Prediction **Result May Vary**")
+    except Exception as e:
+        st.error(f"Prediction Failed : {e}")
+
+
